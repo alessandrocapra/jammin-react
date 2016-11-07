30318430f0c57f285ac2c957fbbd742b858e6979
Alessandro Capra <alessandro.cpr@gmail.com>
2016-11-07 14:03:13 +0200

    Installed webpack and all the sass dependencies. Also modified App.js

--- src/Musician.js
+++ src/Musician.js
@@ -1,3 +1,11 @@
-/**
- * Created by ale on 05/11/16.
- */
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '.Musician.css';
+
+class Musician extends Component {
+    render() {
+        return(
+            <img src={logo} alt={musician.name}/>
+        );
+    }
+}
