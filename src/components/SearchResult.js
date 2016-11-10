e4da4829e0c6a3b3768f7a42422802d4ab77681c
Alessandro Capra <alessandro.cpr@gmail.com>
2016-11-10 18:59:42 +0200

    Completed SearchResult and SearchResultPage layouts

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,7 +1,34 @@
 import React, {Component} from 'react';
+import {Row, Col} from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 
 class SearchResult extends Component {
-    
+    render(){
+        return(
+            <Row>
+                <Col xs={4}>
+                    <img src={this.props.image} alt={this.props.username}/>
+                    <button>Contact me!</button>
+                </Col>
+                <Col xs={8}>
+                    <h3>{this.props.name} {this.props.surname}</h3>
+                    <p><FontAwesome name="rocket"/> {this.props.location} Location </p>
+                    <p><FontAwesome name="rocket"/> {this.props.playedWith} Played with </p>
+                    <p>{this.props.about}about</p>
+                    <Row>
+                        <Col xs={6}>
+                            <h4>Music I play</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aspernatur, at culpa deleniti, dolorum earum est ex, itaque nobis nostrum obcaecati officia quas qui quibusdam quidem quis quos sequi sint</p>
+                        </Col>
+                        <Col xs={6}>
+                            <h4>Musical influences</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti doloremque eveniet excepturi libero minus nihil odio optio quod reiciendis sequi! Amet aspernatur natus nemo obcaecati, quo repellendus reprehenderit rerum unde.</p>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        );
+    }
 }
 
 export default SearchResult;
--- src/components/SearchResultPage.js
+++ src/components/SearchResultPage.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 import { Row, Col } from 'react-bootstrap';
+import Autocomplete from 'react-google-autocomplete';
+import {InputRange} from 'react-input-range'
 
 // Load components
 import SearchResultList from './SearchResultList';
 
 class SearchResultPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            // add here the filter inputs
+            value: 7,
+        };
+    }
+
+    handleValueChange(component, value) {
+        this.setState({
+            value: value,
+        });
+    }
+
     render(){
         return (
             <div>
@@ -12,8 +28,45 @@ class SearchResultPage extends Component {
                     <Col xs={12}><h2>Search results for {this.props.route.instrument} around {this.props.route.location}</h2></Col>
                 </Row>
                 <Row>
-                    <Col xs={4}>
+                    <Col xs={4} className="filter">
                         <h3>Filters</h3>
+                        <Autocomplete
+                            onPlaceSelected={(place) => {
+                                console.log(place);
+                            }}
+                            types={['(regions)']} />
+                        <input type="text" placeholder="Instruments"/>
+
+                        <h4>Availability</h4>
+                        <input name="availability" type="range" defaultValue={50} />
+
+                        <Row>
+                            <Col xs={6}>
+                                <h4>Music played</h4>
+                                {/* This list should be updated taking the info from the profile currently listed in the search results */}
+                                <input type="checkbox" name="musicPlayed"/> Ostia <br/>
+                                <input type="checkbox" name="musicPlayed"/> Ostia <br/>
+                                <input type="checkbox" name="musicPlayed"/> Ostia <br/>
+                                <input type="checkbox" name="musicPlayed"/> Ostia <br/>
+                            </Col>
+                            <Col xs={6}>
+                                <h4>Music listened</h4>
+                                {/* This list should be updated taking the info from the profile currently listed in the search results */}
+                                <input type="checkbox" name="musicListened"/> Ostia <br/>
+                                <input type="checkbox" name="musicListened"/> Ostia <br/>
+                                <input type="checkbox" name="musicListened"/> Ostia <br/>
+                                <input type="checkbox" name="musicListened"/> Ostia <br/>
+
+                            </Col>
+                        </Row>
+
+                        <h4>Reviews</h4>
+                        {/* This list should be updated taking the info from the profile currently listed in the search results */}
+                        <input type="checkbox" name="review"/> Rockstar <br/>
+                        <input type="checkbox" name="review"/> Super <br/>
+                        <input type="checkbox" name="review"/> OK <br/>
+                        <input type="checkbox" name="review"/> Not good <br/>
+
                     </Col>
                     <Col xs={8}>
                         <SearchResultList />
