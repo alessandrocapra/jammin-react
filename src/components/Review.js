7c3606cfd31e3b0ef4d7f62f4a849b1f7376411e
Alessandro Capra <alessandro.cpr@gmail.com>
2016-11-12 18:18:16 +0200

    Fixed small details on review

--- src/components/Review.js
+++ src/components/Review.js
@@ -10,10 +10,11 @@ class Review extends Component {
                     <h4>{this.props.title}</h4>
                     <p><strong>{this.props.instrument}</strong></p>
                 </header>
-                <p>Lorem ipsum dolor set amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p>Lorem ipsum dolor set amet, consectetur adipisicing, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 <footer>
                     <p>Review by <a href="#"><strong>{this.props.name}</strong></a></p>
                 </footer>
+                <hr />
             </article>
         );
     }
