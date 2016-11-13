13f1df320fe4a8dd0c43a8b815246ba61ce03c4d
Alessandro Capra <alessandro.cpr@gmail.com>
2016-11-13 15:53:06 +0200

    Fixed some responsive elements, in the venue and home pages.

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -8,21 +8,21 @@ class Venue extends Component{
     render(){
         return (
             <Row className="venue">
-                <Col xs={5}>
+                <Col xs={12} sm={5}>
                     <img src={this.props.image} alt={this.props.name}/>
                 </Col>
-                <Col xs={7}>
+                <Col xs={12} sm={7}>
                     <h2>{this.props.name}</h2>
                     <div className="details">
                         <span><FontAwesome name="globe"/> {this.props.location} </span>
                         <span><FontAwesome name="envelope"/> {this.props.contact}</span>
                     </div>
                     <Row>
-                        <Col xs={7}>
+                        <Col xs={12} sm={7}>
                             <h3>Description</h3>
                             <p>Lorem poret dolor sit amet, consectetur adipisicing elit. Aliquid animi blanditiis cumque deserunt dicta distinctio, eaque excepturi explicabo id iusto labore nemo numquam perferendis quo quod reiciendis soluta tempora veniam!</p>
                         </Col>
-                        <Col xs={5}>
+                        <Col xs={12} sm={5}>
                             <h3>Services</h3>
                             <ul>
                                 <li>One</li>
