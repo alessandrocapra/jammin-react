fe47c8ff15c9daa92d4ede0c16159bbd38ca3650
Alessandro Capra <alessandro.cpr@gmail.com>
2016-11-22 20:27:14 +0200

    Added reference to Github issue to adapt to React

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import NavLink from './NavLink';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 class App extends Component {
-
     render() {
         return(
             <Grid className="contenitore">
