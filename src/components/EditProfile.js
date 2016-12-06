6a010fc662c1aebcb9f796f77bff0badfbe20116
Alessandro Capra <alessandro.cpr@gmail.com>
2016-12-06 15:32:13 +0200

    Added location and about sections to Edit page

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import {Row,Col} from 'react-bootstrap';
 import firebase from 'firebase';
+import Autocomplete from 'react-google-autocomplete';
 
 class Register extends Component {
     constructor(props){
         super(props);
         this.state = {
             user: {},
-        }
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+
     }
 
     componentWillMount() {
@@ -20,6 +24,12 @@ class Register extends Component {
             console.log(user);
             this.setState({user: user});
         }.bind(this));
+    }
+
+    handleChange(e){
+        this.setState({user : { ...this.state.user, [e.target.name]: e.target.value}});
+        console.log('name: value', e.target.name, e.target.value);
+
     }
 
     render() {
@@ -38,10 +48,27 @@ class Register extends Component {
                         <h2>My info</h2>
                         <div className="form-group">
                             <label htmlFor="name">
-                                Name <input type="text" name="name" value={this.state.user.name}/>
+                                Name <input type="text" name="name" value={this.state.user.name} onChange={this.handleChange}/>
                             </label>
                             <label htmlFor="surname">
-                                Surname <input type="text" name="surname" value={this.state.user.surname}/>
+                                Surname <input type="text" name="surname" value={this.state.user.surname} onChange={this.handleChange}/>
+                            </label>
+                            <label htmlFor="age">
+                                Age <input type="text" name="age" placeholder="e.g. 23" value={this.state.user.age} onChange={this.handleChange}/>
+                            </label>
+                            <label htmlFor="age">
+                                Location
+                                <Autocomplete
+                                onPlaceSelected={(place) => {
+                                    console.log(place);
+                                    this.setState({user: { ...this.state.user, location: place.name}});
+                                }}
+                                types={['(regions)']} value={this.state.user.location} onChange={this.handleChange}
+                                className="autocompleteLocation" />
+                            </label>
+
+                            <label htmlFor="about">
+                                About me <textarea name="about" id="" cols="30" rows="10" placeholder="Present yourself to other musicians!">{this.state.user.about}</textarea>
                             </label>
                         </div>
                     </Col>
