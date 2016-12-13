a7e4eee55c94bbd999ad5cc46c3f4f188e358a9e
Alessandro Capra <alessandro.cpr@gmail.com>
2016-12-13 14:36:22 +0200

    Search now works both by location and instrument played

--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Row} from 'react-bootstrap';
 import {FBAppDB} from '../modules/firebase';
+import update from 'immutability-helper';
 
 
 import SearchResult from './SearchResult';
@@ -9,7 +10,7 @@ class SearchResultList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            users: {}
+            users: []
         }
     }
 
@@ -17,27 +18,33 @@ class SearchResultList extends Component {
         const users = FBAppDB.ref('users');
 
         let location = this.props.location;
-        let instrument = this.props.instrument;
+        let instrumentProp = this.props.instrument;
 
         let searchResult = users.orderByChild('location').equalTo(location).on('value', (snapshot) => {
             let profilesArray = snapshot.val();
-            this.setState({ users: {...profilesArray}});
-            // ...
+            Object.keys(profilesArray).map((profile) => {
+                let instrumentsArray = profilesArray[profile].instruments;
+                let instrumentsName = [];
+                if(instrumentsArray.length){
+                    instrumentsArray.map((instrument) => {
+                        // if one of the instruments is the one in the search, add the profile to the component state
+                        if(instrument.name == instrumentProp.toLowerCase()){
+                            this.setState({users: update(this.state.users, {$push: [profilesArray[profile]]})});
+                        }
+                    });
+                } else {
+                    console.log('Error: no profiles with instruments available!');
+                }
+            });
         });
-
-        // let profiles = [];
-        // users.orderByChild('location').on('value', (snap) => {
-        //     let users = snap.val();
-        //     console.log('users from search results: ', users);
-        //     const profiles = Object.keys(users).map((key) => users[key]);
-        //     this.setState({ users: {...profiles}});
-        // });
     }
 
     render(){
+
         return(
             <Row>
                 {Object.keys(this.state.users).map((key) => {
+                    console.log('what is here?: ', key);
                     return <SearchResult user={this.state.users[key]} key={key} />
                 })}
             </Row>
