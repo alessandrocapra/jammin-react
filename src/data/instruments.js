f0f4b5154c12389fe4dad5490c9a3e44bb1b8ce5
milda norkute <menulio.kiskis@gmail.com>
2016-12-19 01:46:20 +0200

    added more instruments but need to update their images

--- src/data/instruments.js
+++ src/data/instruments.js
@@ -13,6 +13,61 @@ const InstrumentList = [
 			"image" : "/img/instruments/electric-guitar.svg",
             "value" : "electric-guitar",
             "label" : "Electric guitar",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "acoustic-guitar",
+            "label" : "Acoustic guitar",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "violin",
+            "label" : "Violin",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "piano",
+            "label" : "Piano",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "trumpet",
+            "label" : "Trumpet",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "trombone",
+            "label" : "Trombone",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "tuba",
+            "label" : "Tuba",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "french-horn",
+            "label" : "French Horn",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "clarinet",
+            "label" : "Clarinet",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "saxophone",
+            "label" : "Saxophone",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "flute",
+            "label" : "Flute",
+		},
+		{
+			"image" : "/img/instruments/electric-guitar.svg",
+            "value" : "cello",
+            "label" : "Cello",
 		}
 	];
 
