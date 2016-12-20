c44dcaa2716d85b1d2e7eb99f976afec3f0ce737
Stefano <ssteee@gmail.com>
2016-12-20 03:25:26 +0200

    Combine search filters

--- src/components/SearchResultPage.js
+++ src/components/SearchResultPage.js
@@ -131,35 +131,58 @@ class SearchResultPage extends Component {
 
     render(){
 
-        var influences_selected = [];
-        $(document).ready(function() {
-            $('#music_listen :checkbox').change(function() {
+        var filters_selected = [];
+        $(document).ready(() => {
+            $('#filters1 :checkbox, #filters2 :checkbox').change(function() {
 
+                // Get checkbox values
                 var selected = this.name;
                 if (this.checked) {
-                    influences_selected.push(selected);
+                    filters_selected.push(selected);
                 } else {
-                    var index = influences_selected.indexOf(selected);
-                    influences_selected.splice(index, 1);
+                    // Remove filter from array
+                    var index = filters_selected.indexOf(selected);
+                    filters_selected.splice(index, 1);
                 }
 
-                if (influences_selected.length == 0) {
-                    $('.music_influence').each(function () {
+                // Check correspondence between checkbox values
+                // and filter_options
+                if (filters_selected.length == 0) {
+                    // Show all elements -- no filter applied
+                    $('.filter_options').each(function () {
                         $(this).parents().eq(3).css('display', 'block');
                     });
                 } else {
-                    $('.music_influence').each(function () {
+                    $('.result_info').each(function () {
 
                         var influences_matching = 0;
-                        for (var influence in influences_selected) {
-                            $(this).children('.tag').each(function () {
-                                if ($(this).text() == influences_selected[influence]) {
+                        for (var influence in filters_selected) {
+
+                            var user_filter_options = [];
+                            $(this).find('.tag').eq(0).each(function(){
+                                user_filter_options.push($(this).text());
+                            });
+
+                            $(this).find('.tag').eq(1).each(function(){
+                                user_filter_options.push($(this).text());
+                            });
+                            console.log(user_filter_options);
+
+                            for (var user_filter_option in user_filter_options){
+                                // console.log(user_filter_options[user_filter_option])
+                                if (user_filter_options[user_filter_option] == filters_selected[influence]){
                                     influences_matching += 1;
                                 }
-                            });
+                            }
+                            // user_filter_options.each(function () {
+                            //     console.log(this);
+                            //     if (this == filters_selected[influence]) {
+                            //         influences_matching += 1;
+                            //     }
+                            // });
                         }
 
-                        if (influences_matching == influences_selected.length) {
+                        if (influences_matching == filters_selected.length) {
                             $(this).parents().eq(3).css('display', 'block');
                         } else {
                             $(this).parents().eq(3).css('display', 'none');
@@ -167,43 +190,81 @@ class SearchResultPage extends Component {
                     })
                 }
             });
-
-            var music_play_selected = [];
-            $('#music_play :checkbox').change(function () {
-
-                var selected = this.name;
-                if (this.checked) {
-                    music_play_selected.push(selected);
-                } else {
-                    var index = music_play_selected.indexOf(selected);
-                    music_play_selected.splice(index, 1);
-                }
-
-                if (music_play_selected.length == 0) {
-                    $('.music_play').each(function () {
-                        $(this).parents().eq(3).css('display', 'block');
-                    });
-                } else {
-                    $('.music_play').each(function () {
-
-                        var music_play_matching = 0;
-                        for (var music_play in music_play_selected) {
-                            $(this).children('.tag').each(function () {
-                                if ($(this).text() == music_play_selected[music_play]) {
-                                    music_play_matching += 1;
-                                }
-                            });
-                        }
-
-                        if (music_play_matching == music_play_selected.length) {
-                            $(this).parents().eq(3).css('display', 'block');
-                        } else {
-                            $(this).parents().eq(3).css('display', 'none');
-                        }
-                    })
-                }
-            })
         });
+
+        {/*var influences_selected = [];*/}
+        {/*$(document).ready(function() {*/}
+            {/*$('#music_listen :checkbox').change(function() {*/}
+
+                {/*var selected = this.name;*/}
+                {/*if (this.checked) {*/}
+                    {/*influences_selected.push(selected);*/}
+                {/*} else {*/}
+                    {/*var index = influences_selected.indexOf(selected);*/}
+                    {/*influences_selected.splice(index, 1);*/}
+                {/*}*/}
+
+                {/*if (influences_selected.length == 0) {*/}
+                    {/*$('.music_influence').each(function () {*/}
+                        {/*$(this).parents().eq(3).css('display', 'block');*/}
+                    {/*});*/}
+                {/*} else {*/}
+                    {/*$('.music_influence').each(function () {*/}
+
+                        {/*var influences_matching = 0;*/}
+                        {/*for (var influence in influences_selected) {*/}
+                            {/*$(this).children('.tag').each(function () {*/}
+                                {/*if ($(this).text() == influences_selected[influence]) {*/}
+                                    {/*influences_matching += 1;*/}
+                                {/*}*/}
+                            {/*});*/}
+                        {/*}*/}
+
+                        {/*if (influences_matching == influences_selected.length) {*/}
+                            {/*$(this).parents().eq(3).css('display', 'block');*/}
+                        {/*} else {*/}
+                            {/*$(this).parents().eq(3).css('display', 'none');*/}
+                        {/*}*/}
+                    {/*})*/}
+                {/*}*/}
+            {/*});*/}
+
+            {/*var music_play_selected = [];*/}
+            {/*$('#music_play :checkbox').change(function () {*/}
+
+                {/*var selected = this.name;*/}
+                {/*if (this.checked) {*/}
+                    {/*music_play_selected.push(selected);*/}
+                {/*} else {*/}
+                    {/*var index = music_play_selected.indexOf(selected);*/}
+                    {/*music_play_selected.splice(index, 1);*/}
+                {/*}*/}
+
+                {/*if (music_play_selected.length == 0) {*/}
+                    {/*$('.music_play').each(function () {*/}
+                        {/*$(this).parents().eq(3).css('display', 'block');*/}
+                    {/*});*/}
+                {/*} else {*/}
+                    {/*$('.music_play').each(function () {*/}
+
+                        {/*var music_play_matching = 0;*/}
+                        {/*for (var music_play in music_play_selected) {*/}
+                            {/*$(this).children('.tag').each(function () {*/}
+                                {/*if ($(this).text() == music_play_selected[music_play]) {*/}
+                                    {/*music_play_matching += 1;*/}
+                                {/*}*/}
+                            {/*});*/}
+                        {/*}*/}
+
+        //                 if (music_play_matching == music_play_selected.length) {
+        //                     $(this).parents().eq(3).css('display', 'block');
+        //                 } else {
+        //                     $(this).parents().eq(3).css('display', 'none');
+        //                 }
+        //             })
+        //         }
+        //     })
+        // });
 
         return (
             <div className="searchResultPage">
@@ -264,7 +325,7 @@ class SearchResultPage extends Component {
                                 <label>
                                     Influences
                                 </label>
-                                <div id="music_listen">
+                                <div id="filters1">
                                 {this.state.music_listen.length ? this.state.music_listen.map((artist) => {
                                     return Object.keys(artist).map(function(artist_name){
                                         return <div><input type="checkbox" name={artist_name}/> {artist_name} ({artist[artist_name]}) </div>;
@@ -276,7 +337,7 @@ class SearchResultPage extends Component {
                                 <label>
                                     Artists listened
                                 </label>
-                                <div id="music_play">
+                                <div id="filters2">
                                 {this.state.music_play.length ? this.state.music_play.map((artist) => {
                                     return Object.keys(artist).map(function (artist_name) {
                                         return <div><input type="checkbox" name={artist_name}/> {artist_name} ({artist[artist_name]}) </div>;
