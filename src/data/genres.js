bc00af595f14e8700649747dc7145948d2cc9bef
milda norkute <menulio.kiskis@gmail.com>
2016-12-24 15:50:24 +0200

    added more music genres to the list

--- src/data/genres.js
+++ src/data/genres.js
@@ -15,8 +15,48 @@ const GenreList = [
 
     },
     {
+        "value": "Country",
+        "label": "Country",
+
+    },
+    {
+        "value": "Dance Music",
+        "label": "Dance Music",
+
+    },
+    {
+        "value": "Electronic",
+        "label": "Electronic",
+
+    },
+    {
+        "value": "Electroswing",
+        "label": "Electroswing",
+
+    },
+    {
+        "value": "Experimental Rock",
+        "label": "Experimental Rock",
+
+    },
+    {
+        "value": "Folk Punk",
+        "label": "Folk Punk",
+
+    },
+    {
         "value": "Folk Rock",
         "label": "Folk Rock",
+
+    },
+    {
+        "value": "Funk",
+        "label": "Funk",
+
+    },
+    {
+        "value": "Goth",
+        "label": "Goth",
 
     },
     {
@@ -25,8 +65,108 @@ const GenreList = [
 
     },
     {
+        "value": "Hardcore Punk",
+        "label": "Hardcore Punk",
+
+    },
+    {
+        "value": "Hard Rock",
+        "label": "Hard Rock",
+
+    },
+    {
+        "value": "Indie Pop",
+        "label": "Indie Pop",
+
+    },
+    {
         "value": "Indie Rock",
         "label": "Indie Rock",
+
+    },
+    {
+        "value": "Jazz",
+        "label": "Jazz",
+
+    },
+    {
+        "value": "Latin",
+        "label": "Latin",
+
+    },
+    {
+        "value": "Metal",
+        "label": "Metal",
+
+    },
+    {
+        "value": "New Wave",
+        "label": "New Wave",
+
+    },
+    {
+        "value": "Opera",
+        "label": "Opera",
+
+    },
+    {
+        "value": "Pop",
+        "label": "Pop",
+
+    },
+    {
+        "value": "Progressive Rock",
+        "label": "Progressive Rock",
+
+    },
+    {
+        "value": "Punk",
+        "label": "Punk",
+
+    },
+    {
+        "value": "R&B",
+        "label": "R&B",
+
+    },
+    {
+        "value": "Reggae",
+        "label": "Reggae",
+
+    },
+    {
+        "value": "Reggaeton",
+        "label": "Reggaeton",
+
+    },
+    {
+        "value": "Rock",
+        "label": "Rock",
+
+    },
+    {
+        "value": "Steampunk",
+        "label": "Steampunk",
+
+    },
+    {
+        "value": "Salsa",
+        "label": "Salsa",
+
+    },
+    {
+        "value": "Soul",
+        "label": "Soul",
+
+    },
+    {
+        "value": "Techno",
+        "label": "Techno",
+
+    },
+    {
+        "value": "Trance",
+        "label": "Trance",
 
     },
 			// "Alternative Rock",
