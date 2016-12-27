de3247976085ad48d4c81330a60abc317118d65e
Alessandro Capra <alessandro.cpr@gmail.com>
2016-12-27 13:14:25 +0100

    Added groups on homepage for logged in users, fixed FAQ page

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -11,17 +11,15 @@ class Faq extends Component {
                 </Col>
                 <Col xs={12}>
                     <Row>
-                        <Col xs={12} sm={6}>
+                        <Col xs={12} sm={4}>
                             <h3> I am an unregistered user. How come I cannot contact anyone? </h3>
                             <p> Hi and welcome to Jammin. Our policy is that only registered users can contact musicians registered on Jammin. Creating a profile is free, so why not do it today? This will also allow other musicians to find you.</p>
                         </Col>
-                        <Col xs={12} sm={6}>
+                        <Col xs={12} sm={4}>
                             <h3> I am a beginner musician. Is Jammin the right place for me? </h3>
                             <p>Jammin welcomes musicians of all levels, so of course. Besides, you can only get better by practising. </p>
                         </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12} sm={6}>
+                        <Col xs={12} sm={4}>
                             <h3>What if someone leaves a bad review on my profile, after playing with me?</h3>
                             <p>The reviews have been introduced as an additional measure to help express one’s ability to play a music instrument. All reviews are reviewed by Jammin staff to ensure they are written in appropriate language. Besides pure community is very friendly, so you shouldn’t worry too much.</p>
                         </Col>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 // Example to import element from React-Bootstrap
 import { Row, Col } from 'react-bootstrap';
@@ -9,6 +10,151 @@ import SearchForm from './SearchForm';
 class Home extends Component {
 
     render() {
+        let homeContent = null;
+
+        if(!firebase.auth().currentUser){
+            // user not logged in
+            homeContent =
+                <div>
+                    <Row>
+                        <Col xs={12} className="h3homepageclass">
+                            <h2 className="text-center"> How Jammin works </h2>
+                        </Col>
+                    </Row>
+                    <Row className="facts">
+                        <Row>
+                            <Col xs={12} className="imageclass">
+                                <div className="module">
+                                    <h3>About Jammin</h3>
+                                    <p>Jammin is a community of musicians and musical artists.</p> <p> Joining Jammin is free and always will be.</p>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={12} className="imageclass">
+                                <div className="module two">
+                                    <h3>Find musicians</h3>
+                                    <p>Create a free profile, find musicians, contact them, form or join jammer groups to play music together.</p>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={12} className="imageclass">
+                                <div className="module three">
+                                    <h3>Meet and Play</h3>
+                                    <p>Find a venue suggestion on our website, contact them, meet up, play and enjoy!</p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Row>;
+                </div>
+        } else {
+            homeContent =
+                <div>
+                    <Row>
+                        <Col xs={12} className="h3homepageclass">
+                            <h2 className="text-center"> Some groups suggested for you </h2>
+                        </Col>
+                    </Row>
+                    <Row className="groups">
+                        <Col xs={12} sm={3}>
+                            <div className="profile-pic">
+                                <div className="image" style={{'background' : 'url(img/bonham.jpg)', 'backgroundSize' : 'cover', 'backgroundRepeat' : 'no-repeat'}}/>
+                            </div>
+                            <h3>John Bonham</h3>
+                            <div className="container-elements">
+                                <img src="img/instruments/drum-set.svg" alt="Drums"/>
+                                <div style={{"display" : "inline-block"}}>
+                                    <h4>Drums</h4>
+                                    <p>9 years of experience</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={3}>
+                            <div className="profile-pic">
+                                <div className="image" style={{'background' : 'url(img/hendrix.jpeg)', 'backgroundSize' : 'cover', 'backgroundRepeat' : 'no-repeat'}}/>
+                            </div>
+                            <h3>Jimi Hendrix</h3>
+                            <div className="container-elements">
+                                <img src="img/instruments/electric-guitar-1.svg" alt="Electric guitar"/>
+                                <div style={{"display" : "inline-block"}}>
+                                    <h4>Electric guitar</h4>
+                                    <p>11 years of experience</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={3}>
+                            <div className="profile-pic">
+                                <div className="image" style={{'background' : 'url(img/entwistle.jpg)', 'backgroundSize' : 'cover', 'backgroundRepeat' : 'no-repeat'}}/>
+                            </div>
+                            <h3>John Entwistle</h3>
+                            <div className="container-elements">
+                                <img src="img/instruments/electric-guitar.svg" alt="Bass guitar"/>
+                                <div style={{"display" : "inline-block"}}>
+                                    <h4>Bass guitar</h4>
+                                    <p>5 years of experience</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={3}>
+                            <div className="profile-pic">
+                                <div className="image" style={{'background' : 'url(img/avatar.png)', 'backgroundSize' : 'cover', 'backgroundRepeat' : 'no-repeat'}}/>
+                            </div>
+                            <h3>This could be you!</h3>
+                            <button>Join this group</button>
+                        </Col>
+                    </Row>
+                    <Row className="groups">
+                        <Col xs={12} sm={3}>
+                            <div className="profile-pic">
+                                <div className="image" style={{'background' : 'url(img/bonham.jpg)', 'backgroundSize' : 'cover', 'backgroundRepeat' : 'no-repeat'}}/>
+                            </div>
+                            <h3>John Bonham</h3>
+                            <div className="container-elements">
+                                <img src="img/instruments/drum-set.svg" alt="Drums"/>
+                                <div style={{"display" : "inline-block"}}>
+                                    <h4>Drums</h4>
+                                    <p>9 years of experience</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={3}>
+                            <div className="profile-pic">
+                                <div className="image" style={{'background' : 'url(img/hendrix.jpeg)', 'backgroundSize' : 'cover', 'backgroundRepeat' : 'no-repeat'}}/>
+                            </div>
+                            <h3>Jimi Hendrix</h3>
+                            <div className="container-elements">
+                                <img src="img/instruments/electric-guitar-1.svg" alt="Electric guitar"/>
+                                <div style={{"display" : "inline-block"}}>
+                                    <h4>Electric guitar</h4>
+                                    <p>11 years of experience</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={3}>
+                            <div className="profile-pic">
+                                <div className="image" style={{'background' : 'url(img/entwistle.jpg)', 'backgroundSize' : 'cover', 'backgroundRepeat' : 'no-repeat'}}/>
+                            </div>
+                            <h3>John Entwistle</h3>
+                            <div className="container-elements">
+                                <img src="img/instruments/electric-guitar.svg" alt="Bass guitar"/>
+                                <div style={{"display" : "inline-block"}}>
+                                    <h4>Bass guitar</h4>
+                                    <p>5 years of experience</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={3}>
+                            <div className="profile-pic">
+                                <div className="image" style={{'background' : 'url(img/avatar.png)', 'backgroundSize' : 'cover', 'backgroundRepeat' : 'no-repeat'}}/>
+                            </div>
+                            <h3>This could be you!</h3>
+                            <button>Join this group</button>
+                        </Col>
+                    </Row>
+                </div>
+        }
+
         return (
             <div>
                 <Row>
@@ -17,37 +163,7 @@ class Home extends Component {
                         <SearchForm instruments={this.props.route.instruments} />
                     </Col>
                 </Row>
-                <Row>
-                    <Col xs={12} className="h3homepageclass">
-                    <h2 className="text-center"> How Jammin works </h2>
-                    </Col>
-                </Row>
-                <Row className="facts">
-                <Row>
-                    <Col xs={12} className="imageclass">
-                        <div className="module">
-                        <h3>About Jammin</h3>
-                            <p>Jammin is a community of musicians and musical artists.</p> <p> Joining Jammin is free and always will be.</p>
-                        </div>
-                    </Col>
-                    </Row>
-                    <Row>
-                    <Col xs={12} className="imageclass">
-                        <div className="module two">
-                        <h3>Find musicians</h3>
-                        <p>Create a free profile, find musicians, contact them, form or join jammer groups to play music together.</p>
-                        </div>
-                    </Col>
-                    </Row>
-                    <Row>
-                    <Col xs={12} className="imageclass">
-                        <div className="module three">
-                        <h3>Meet and Play</h3>
-                        <p>Find a venue suggestion on our website, contact them, meet up, play and enjoy!</p>
-                        </div>
-                    </Col>
-                    </Row>
-                </Row>
+                {homeContent}
             </div>
         );
     }
